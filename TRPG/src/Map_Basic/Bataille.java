1d0da8ac55b0d99f5d5dc74c1839276f3cbc0ae8
jgpga <jgpga@LAPTOP-GPD6LFAA>
2017-11-12 21:06:40 +0100

    first commit
    Change-Id: I36093e9d70a007abcbe668a5b1a7c3c14298dfbd

--- TRPG/src/Map_Basic/Bataille.java
+++ TRPG/src/Map_Basic/Bataille.java
@@ -3,17 +3,11 @@ package Map_Basic;
 public class Bataille {
 
 	public static void main(String[] args) {
+		
 		// TODO Auto-generated method stub
 		// modif
 		System.out.println("lol");
-		// autre modif
-		// modif ju 2
-		// autre modif 2
-		// autre modif
-		// autre modif 2
-		// autre modif 3
-		// Zarastoustra c'est vraimment bien !
-
+		
 	}
 
 }
