a9ce5293e064555cb98a781b71b620353d7776bf
jgpga <jgpga@LAPTOP-JGPG-D6LFAA>
2017-11-26 18:31:04 +0100

    Change-Id: Ia3ccdc527f67c1e12804b7215f0cf678b2f9ecac

--- TRPG/src/Map_Basic/Bataille.java
+++ TRPG/src/Map_Basic/Bataille.java
@@ -4,9 +4,9 @@ public class Bataille {
 
 	public static void main(String[] args) {
 		
-		// TODO Auto-generated method stub
-		System.out.println("lol");
-		//SAP
+		// TODO Auto-generated method stub	
+		System.out.println("lol");		
+		// SAP
 	}
 
 }
